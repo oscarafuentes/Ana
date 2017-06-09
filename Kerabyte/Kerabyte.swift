02417818cb38163481d1443d2a16dd294810ac15
Oscar Fuentes <oscar@appwinit.com>
2017-06-08 21:49:39 -0400

    Added empty base class.

---
+++ Kerabyte/Kerabyte.swift
@@ -0,0 +1,13 @@
+//
+//  Kerabyte.swift
+//  Kerabyte
+//
+//  Created by Oscar Fuentes on 6/8/17.
+//  Copyright © 2017 Oscar Fuentes. All rights reserved.
+//
+
+import Foundation
+
+public final class Kerabyte {
+    
+}
