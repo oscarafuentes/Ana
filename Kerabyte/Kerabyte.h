2d815433bc712573b52d291e11b02c39e3380639
Oscar Fuentes <oscar@appwinit.com>
2017-06-08 21:47:30 -0400

    Initial Commit

---
+++ Kerabyte/Kerabyte.h
@@ -0,0 +1,19 @@
+//
+//  Kerabyte.h
+//  Kerabyte
+//
+//  Created by Oscar Fuentes on 6/8/17.
+//  Copyright © 2017 Oscar Fuentes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kerabyte.
+FOUNDATION_EXPORT double KerabyteVersionNumber;
+
+//! Project version string for Kerabyte.
+FOUNDATION_EXPORT const unsigned char KerabyteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kerabyte/PublicHeader.h>
+
+
