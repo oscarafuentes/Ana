9f63dd25a66cb45ca7a1d4c811047eb4f1fb00d4
Oscar Fuentes <oscar@appwinit.com>
2017-06-10 00:01:30 -0400

    Connected code-based logic to storyboard example.

---
+++ Example/Templates/Index/IndexTemplate.swift
@@ -0,0 +1,14 @@
+//
+//  IndexTemplate.swift
+//  Kerabyte
+//
+//  Created by Oscar Fuentes on 6/8/17.
+//  Copyright © 2017 Oscar Fuentes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class IndexTemplate: UIViewController {
+    
+}
