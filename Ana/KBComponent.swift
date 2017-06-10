218502c14d279336a05fc7f5b8741e866d49c57f
Oscar Fuentes <oscar@appwinit.com>
2017-06-10 19:24:27 -0400

    Began renaming.

---
+++ Ana/KBComponent.swift
@@ -0,0 +1,42 @@
+//
+//  KBComponent.swift
+//  Kerabyte
+//
+//  Created by Oscar Fuentes on 6/8/17.
+//  Copyright © 2017 Oscar Fuentes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class KBComponent: NSObject {
+    
+    public private(set) var template: UIResponder!
+    
+    public override init() {
+        super.init()
+        self.template = self.generateTemplate()
+        self.template.component = self
+    }
+    
+    open func configure() {
+        
+    }
+    
+    open func generateTemplate() -> UIResponder {
+        return UIResponder()
+    }
+    
+    /**
+     Lifecycle Hooks
+     */
+    
+    open func onInit() {
+        
+    }
+    
+    open func onDestroy() {
+        
+    }
+    
+}
