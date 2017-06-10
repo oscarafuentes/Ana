7f307d498b34ecde0d4e8006d033e4bcbe11667c
Oscar Fuentes <oscar@appwinit.com>
2017-06-10 10:34:36 -0400

    Whitespace.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let router = KBRouter(IndexRoute())
         
         Kerabyte.register(router)
-                
+        
         return true
     }
 
