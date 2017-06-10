0e0819b6f1fc582a946da5ff9395792c098e4081
Oscar Fuentes <oscar@appwinit.com>
2017-06-09 21:43:44 -0400

    Cleaned up and added back parameter in router init method.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -15,10 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        let router = KBRouter<IndexRoute>()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {        
+        let router = KBRouter(IndexRoute())
         
         Kerabyte.register(router)
         
--- Kerabyte/KBRouter.swift
+++ Kerabyte/KBRouter.swift
@@ -10,11 +10,11 @@ import Foundation
 
 open class KBRouter<T: KBRoute> {
     
-//    public private(set) var route: KBRoute
+    public private(set) var route: KBRoute
     public private(set) var domains: [String]
     
-    public init(_ domains: [String] = []) {
-//        self.route = route
+    public init(_ route: KBRoute, domains: [String] = []) {
+        self.route = route
         self.domains = domains
     }
     
